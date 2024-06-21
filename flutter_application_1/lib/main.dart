c9280db083874c081abad0a2de94e53ada1a9221
202200030 <127192259+202200030@users.noreply.github.com>
2024-06-22 00:00:36 +0100

    d

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'sporty_home_page.dart';
+import 'customization_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/login', 
       routes: {
-        '/login': (context) => LoginScreen(), 
+        '/login': (context) => CustomizationScreen(), 
         '/home': (context) => SportyHomePage(), 
       },
     );
